07df88114b54b8ffe131536a110c10154844858f
WarriorLeyts <rmagah1996@gmail.com>
2026-06-23 15:51:53 +0300

    fix: wrap useSearchParams in Suspense

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,18 +1,14 @@
 import { prisma } from "@/lib/prisma";
+import { Suspense } from "react";
 import { CatalogClient } from "@/components/catalog/CatalogClient";
 
 export default async function CatalogPage() {
-  const categories = await prisma.category.findMany({
-    orderBy: {
-      name: "asc",
-    },
-  });
+  const categories = await prisma.category.findMany({ orderBy: { name: "asc" } });
+  const brands = await prisma.brand.findMany({ orderBy: { name: "asc" } });
 
-  const brands = await prisma.brand.findMany({
-    orderBy: {
-      name: "asc",
-    },
-  });
-
-  return <CatalogClient categories={categories} brands={brands} />;
+  return (
+    <Suspense>
+      <CatalogClient categories={categories} brands={brands} />
+    </Suspense>
+  );
 }
