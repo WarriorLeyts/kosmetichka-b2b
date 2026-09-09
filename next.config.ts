923568abf374c19d20d2b761629662e05dbaebe7
WarriorLeyts <rmagah1996@gmail.com>
2026-09-09 14:36:57 +0300

    fix login url, mobile layout, categories

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,24 @@ const nextConfig: NextConfig = {
   },
   devIndicators: false,
   async rewrites() {
-    if (process.env.NODE_ENV !== "development") return [];
-    return [
-      {
+    const rules = [];
+
+    if (process.env.NODE_ENV === "development") {
+      rules.push({
         source: "/1c/:path*",
         destination: "https://kosmetichka-opt.ru/1c/:path*",
-      },
-    ];
+      });
+    }
+
+    // На Vercel проксируем картинки с основного VPS
+    if (process.env.VERCEL) {
+      rules.push({
+        source: "/image/:path*",
+        destination: "https://kosmetichka-opt.ru/image/:path*",
+      });
+    }
+
+    return rules;
   },
 };
 
